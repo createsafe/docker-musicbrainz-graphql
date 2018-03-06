c14b37a3046b9b2a324570ed9552f1bcecb98d6e
Benjamin Van Treese <vantreeseba@gmail.com>
2018-03-06 02:01:47 -0600

    Making an init task to start first db

--- db/init-test.sql
+++
@@ -1,3 +0,0 @@
-
-CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40), completed BOOLEAN);
-INSERT INTO items(text, completed) VALUES('test', false);
---
+++ src/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const {postgraphile} = require('postgraphile');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.use(postgraphile('postgres://musicbrainz@db/musicbrainz', 'musicbrainz', {graphiql: true}))
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+}
+console.log(`Running on http://${HOST}:${PORT}`);
+
