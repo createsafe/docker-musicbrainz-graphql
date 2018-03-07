678173585c965a79b39c77ad594b1c31b9d62edb
Benjamin Van Treese <vantreeseba@gmail.com>
2018-03-07 12:51:24 -0600

    Remove unneeded stuff in docker-compose, change dockerfile to add working dir, add cmd to dockerfile

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, HOST);
-}
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log('Running on http://' + HOST + ':' + PORT);
 
+console.log('weee');
