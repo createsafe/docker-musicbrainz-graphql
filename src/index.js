ebb21dd452ef4c11d1844f95999bf679ada3ecf2
Benjamin Van Treese <vantreeseba@gmail.com>
2018-03-08 23:18:07 -0600

    Fix issue with node_modules

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
 
 const express = require('express');
 const {postgraphile} = require('postgraphile');
+const cors = require('cors');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
+app.use(cors);
 app.use(postgraphile('postgres://musicbrainz@db/musicbrainz', 'musicbrainz', {graphiql: true}))
 app.get('/', (req, res) => {
   res.send('Hello world\n');
