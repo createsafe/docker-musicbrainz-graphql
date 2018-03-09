57074dab39d9feed5730388852c07460cc4b97b1
Benjamin Van Treese <vantreeseba@gmail.com>
2018-03-09 13:47:35 -0600

    Add nodemon, fix issue with npm install in web container

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(postgraphile('postgres://musicbrainz@db/musicbrainz', 'musicbrainz', {graphiql: true}))
 app.get('/', (req, res) => {
   res.send('Hello world\n');
